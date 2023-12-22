dc194d5ee97a822bbdf812c892051c4e1b9d9412
AndriiZaikin <1zaikinandrii1@gmail.com>
2023-12-22 18:48:12 +0000

    nearly done

--- Historical_Pricing.py
+++ Historical_Pricing.py
@@ -1,50 +1,193 @@
-#IMPORTS
+import os
+from datetime import datetime
+import time
+import requests
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from typing import List, Tuple
+import json
 
-#Brokerage API Client
-def initialize_api_client(api_key, secret_key):
-    # Implementation details for initializing API client
-    pass
 
-def handle_rate_limits(request_limit):
-    # Implementation details for handling rate limits
-    pass
+class BrokerageAPIClient:
+    def __init__(self, base_url='https://api.brokerage.com'):
+        self.api_key = os.environ.get('API_KEY')
+        self.secret_key = self._get_secret_key_from_vault()
+        self.base_url = base_url
+        self.session = requests.Session()
 
-def input_parameters(instrument, start_date, end_date):
-    # Implementation details for taking instrument, start date, and end date as input
-    pass
+    def _get_secret_key_from_vault(self):
+        # Implementation details for retrieving secret key from a secure vault
+        pass
 
-#Download OHLCV Bars
-def submit_api_request(instrument, start_date, end_date):
-    # Implementation details for submitting API requests and downloading data
-    pass
+    def initialize_api_client(self):
+        if self.authenticate():
+            return True
+        else:
+            return False
 
-def parse_raw_pricing_data(raw_data):
-    # Implementation details for parsing raw pricing data
-    pass
+    def authenticate(self):
+        headers = {'Authorization': f'Bearer {self.api_key}'}
+        response = self.session.get(f'{self.base_url}/authenticate', headers=headers)
 
-#Data Validation
-def check_corrupt_data(pricing_data):
-    # Implementation details for checking corrupt or missing data
-    pass
+        return response.status_code == 200
 
-def identify_invalid_values(pricing_data):
-    # Implementation details for identifying invalid values
-    pass
+    def input_parameters(self, instrument, start_date, end_date):
+        if not isinstance(instrument, str):
+            raise ValueError("Instrument must be a string.")
+        if not isinstance(start_date, datetime.date):
+            raise ValueError("Start date must be a valid date.")
+        if not isinstance(end_date, datetime.date):
+            raise ValueError("End date must be a valid date.")
+        self.instrument = instrument
+        self.start_date = start_date
+        self.end_date = end_date
 
-def log_warnings_errors(warnings, errors):
-    # Implementation details for logging warnings and errors
-    pass
 
-#Data Storage
-def serialize_to_cloud_storage(pricing_data, date, instrument):
-    # Implementation details for serializing data to cloud storage
-    pass
+class OHLCVBarsDownloader:
+    def __init__(self, api_client):
+        self.api_client = api_client
+        self.raw_pricing_data = None
 
-def support_incremental_updates(existing_data, new_data):
-    # Implementation details for supporting incremental updates
-    pass
+    def submit_api_request(self, instrument):
+        # Implementation details for submitting API requests and downloading data
+        response = requests.get(f"{self.api_client.base_url}/pricing/{instrument}", headers={'Authorization': f'Bearer {self.api_client.api_key}'})
+        if response.status_code == 200:
+            self.raw_pricing_data = response.json()
+        else:
+            raise Exception(f"Error fetching data: {response.status_code}")
 
-#Main Workflow
-def main(instruments, start_date, end_date):
-    # Implementation details for orchestrating the workflow
-    pass
+
+    def parse_raw_pricing_data(self):
+        # Implementation details for parsing raw pricing data
+        parsed_data = []
+        for item in self.raw_pricing_data:
+            parsed_item = {
+                "date": item["date"],
+                "open": item["open"],
+                "high": item["high"],
+                "low": item["low"],
+                "close": item["close"],
+                "volume": item["volume"]
+            }
+            parsed_data.append(parsed_item)
+        return parsed_data
+
+    def download_bars(self, instrument):
+        self.submit_api_request(instrument)
+        parsed_data = self.parse_raw_pricing_data()
+        return parsed_data
+
+class DataValidator:
+    def __init__(self, pricing_data):
+        self.pricing_data = pricing_data
+
+    def check_corrupt_data(self):
+        corrupt_records = []
+        for record in self.pricing_data:
+            if not all(field in record for field in ['date', 'open', 'high', 'low', 'close', 'volume']):
+                corrupt_records.append(record)
+        return corrupt_records
+
+    def identify_invalid_values(self):
+        invalid_values = []
+        for record in self.pricing_data:
+            if record['bid'] > record['ask']:
+                invalid_values.append(record)
+            if record['volume'] == 0:
+                invalid_values.append(record)
+        return invalid_values
+
+    def log_warnings_errors(self, warnings, errors):
+        for record in warnings:
+            print(f"Warning: Invalid data record - {record}")
+        for record in errors:
+            print(f"Error: Corrupt data record - {record}")
+
+
+class DataStorageManager:
+    def __init__(self, connection_string: str, container_name: str, partition_key: str):
+        self.connection_string = connection_string
+        self.container_name = container_name
+        self.partition_key = partition_key
+
+        # Establish connection with the cloud storage
+        self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+        self.container_client = self.blob_service_client.get_container_client(self.container_name)
+
+    def serialize_to_cloud_storage(self, pricing_data: List[dict], date: str, instrument: str):
+        # Implementation details for serializing data to cloud storage
+        # Here we use the date and instrument as a filename in the format of YYYY-MM-DD_Instrument.json
+        filename = f"{date}_{instrument}.json"
+        blob_client = self.container_client.get_blob_client(filename)
+        pricing_data_json_str = json.dumps(pricing_data)
+        blob_client.upload_blob(pricing_data_json_str, overwrite=True)
+
+    def support_incremental_updates(self, existing_data: List[dict], new_data: List[dict]) -> List[dict]:
+        # Implementation details for supporting incremental updates
+        # We use a combination of the date and instrument fields to uniquely identify each piece of data
+        # Here we use the datetime and instrument as the key
+        existing_data_dict = {(item.date, item.instrument): item for item in existing_data}
+        new_data_dict = {(item.date, item.instrument): item for item in new_data}
+
+        # Update the existing data with the new data
+        for key, value in new_data_dict.items():
+            if key in existing_data_dict:
+                existing_data_dict[key] = value
+            else:
+                existing_data_dict[key] = value
+
+        # Convert the updated data back into a list
+        updated_data = list(existing_data_dict.values())
+
+        return updated_data
+    
+
+    #The DataStorageManager class does not explicitly handle data versioning. If you need to store different versions of the data, you can modify the class to include a version number in the filename or as a metadata property.
+
+
+#How to call this function:
+'''from datetime import datetime, timedelta
+from your_module_name import BrokerageAPIClient, OHLCVBarsDownloader, DataValidator, DataStorageManager
+
+# Replace 'your_module_name' with the actual name of the module where you've stored your code.
+
+# Set up your list of instruments and date range
+instruments = ['AAPL', 'GOOGL', 'MSFT']
+start_date = datetime(2023, 1, 1)
+end_date = datetime(2023, 12, 31)
+
+# Initialize the API client
+api_client = BrokerageAPIClient()
+api_client.initialize_api_client()
+api_client.input_parameters(instruments, start_date, end_date)
+
+# Download OHLCV bars
+bars_downloader = OHLCVBarsDownloader(api_client)
+all_pricing_data = {}
+for instrument in instruments:
+    pricing_data = bars_downloader.download_bars(instrument)
+    all_pricing_data[instrument] = pricing_data
+
+# Perform data validation
+validator = DataValidator(all_pricing_data)
+corrupt_data = validator.check_corrupt_data()
+invalid_values = validator.identify_invalid_values()
+
+# Log warnings and errors
+validator.log_warnings_errors(corrupt_data, invalid_values)
+
+# Initialize DataStorageManager with your Azure Storage connection details
+connection_string = 'your_azure_storage_connection_string'
+container_name = 'your_container_name'
+partition_key = 'date_and_instrument'
+storage_manager = DataStorageManager(connection_string, container_name, partition_key)
+
+# Serialize data to cloud storage
+for instrument, pricing_data in all_pricing_data.items():
+    for date, data in pricing_data.items():
+        storage_manager.serialize_to_cloud_storage(data, date, instrument)
+
+# Support incremental updates (assuming you have existing data)
+existing_data = {}  # Replace with your existing data
+for instrument, pricing_data in all_pricing_data.items():
+    existing_data[instrument] = storage_manager.support_incremental_updates(existing_data.get(instrument, []), pricing_data)
+'''
